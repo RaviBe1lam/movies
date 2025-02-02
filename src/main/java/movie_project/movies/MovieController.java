3026a2f5e746036f069b6606ebe03b796345bd18
ravi <ravib.orostechllc.com>
2025-02-02 00:28:55 -0500

    Initial commit

---
+++ src/main/java/movie_project/movies/MovieController.java
@@ -0,0 +1,27 @@
+package movie_project.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping("movies")
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/movie_project/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package movie_project.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
